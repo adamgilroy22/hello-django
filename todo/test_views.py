2ae77605e7b335c77bfd922f4c1c0b732d7070e2
Adam Gilroy <adamgilroy22@gmail.com>
2023-01-03 18:00:12 +0000

    Change initial test so it passes

--- todo/test_views.py
+++ todo/test_views.py
@@ -6,7 +6,7 @@ from .models import Item
 class TestDjango(TestCase):
 
     def test_this_thing_works(self):
-        self.assertEqual(1, 0)
+        self.assertEqual(1, 1)
 
 
 class TestViews(TestCase):
